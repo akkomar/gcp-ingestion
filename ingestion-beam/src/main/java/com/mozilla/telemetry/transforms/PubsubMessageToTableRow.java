27a108dfaa17ab61c7011dae9221d993f49d12fd
Jeff Klukas <jeff@klukas.net>
2019-04-23 10:26:53 -0400

    Transform json map fields to arrays before output to BigQuery
    In parallel to the work on Avro loading, we want to be able to test
    stability and performance of a JSON-based solution.

--- ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/PubsubMessageToTableRow.java
+++ ingestion-beam/src/main/java/com/mozilla/telemetry/transforms/PubsubMessageToTableRow.java
@@ -11,19 +11,28 @@ import com.google.api.services.bigquery.model.TimePartitioning;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.Dataset;
+import com.google.cloud.bigquery.FieldList;
+import com.google.cloud.bigquery.LegacySQLTypeName;
+import com.google.cloud.bigquery.Table;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.mozilla.telemetry.util.Json;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.beam.sdk.coders.KvCoder;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryHelpers;
 import org.apache.beam.sdk.io.gcp.bigquery.TableDestination;
@@ -56,8 +65,9 @@ public class PubsubMessageToTableRow
 
   private final ValueProvider<String> tableSpecTemplate;
 
-  // We'll instantiate this on first use.
+  // We'll instantiate these on first use.
   private transient Cache<DatasetReference, Set<String>> tableListingCache;
+  private transient Cache<TableReference, List<List<String>>> tableMapFieldPathCache;
 
   private PubsubMessageToTableRow(ValueProvider<String> tableSpecTemplate) {
     this.tableSpecTemplate = tableSpecTemplate;
@@ -98,7 +108,9 @@ public class PubsubMessageToTableRow
             .getService();
         Dataset dataset = service.getDataset(ref.getDatasetId());
         if (dataset != null) {
-          dataset.list().iterateAll().forEach(t -> tableSet.add(t.getTableId().getTable()));
+          dataset.list().iterateAll().forEach(t -> {
+            tableSet.add(t.getTableId().getTable());
+          });
         }
         return tableSet;
       });
@@ -115,7 +127,30 @@ public class PubsubMessageToTableRow
       throw new IllegalArgumentException("Resolved destination table does not exist: " + tableSpec);
     }
 
+    // Get and cache a list of paths to map-type fields in this schema.
+    List<List<String>> mapFieldPaths;
+    if (tableMapFieldPathCache == null) {
+      tableMapFieldPathCache = CacheBuilder.newBuilder().expireAfterWrite(Duration.ofMinutes(1))
+          .build();
+    }
+    try {
+      mapFieldPaths = tableMapFieldPathCache.get(ref, () -> {
+        List<List<String>> paths = new ArrayList<>();
+        BigQuery service = BigQueryOptions.newBuilder().setProjectId(ref.getProjectId()).build()
+            .getService();
+        Table table = service.getTable(ref.getDatasetId(), ref.getTableId());
+        if (table != null) {
+          accumulateMapFieldPaths(ImmutableList.of(), table.getDefinition().getSchema().getFields(),
+              paths);
+        }
+        return paths;
+      });
+    } catch (ExecutionException e) {
+      throw new UncheckedExecutionException(e);
+    }
+
     TableRow tableRow = buildTableRow(message.getPayload());
+    transformMapFields(tableRow, mapFieldPaths);
     return KV.of(tableDestination, tableRow);
   }
 
@@ -156,6 +191,69 @@ public class PubsubMessageToTableRow
         }
       }
     }
+  }
+
+  /**
+   * Recursively descend through a list of BQ schema fields, appending paths to discovered
+   * map-type fields.
+   *
+   * @param position a list of parent field names leading up to the passed fields
+   * @param fields all the fields of the current RECORD-type field
+   * @param mapFieldPaths a growing list of paths to all map-type fields
+   */
+  public static void accumulateMapFieldPaths(List<String> position, FieldList fields,
+      List<List<String>> mapFieldPaths) {
+    fields.forEach(field -> {
+      if (LegacySQLTypeName.RECORD.equals(field.getType())) {
+        ArrayList<String> newPosition = new ArrayList<>(position);
+        newPosition.add(field.getName());
+        // Make sure we descend into nested maps first, so that we end up transforming
+        // the deepest maps first.
+        accumulateMapFieldPaths(newPosition, field.getSubFields(), mapFieldPaths);
+        // If this RECORD in the BQ schmea looks like a transformed map, we record the path.
+        if (field.getSubFields().size() == 2 //
+            && field.getSubFields().get(0).getName().equals("key") //
+            && field.getSubFields().get(1).getName().equals("value")) {
+          mapFieldPaths.add(newPosition);
+        }
+      }
+    });
+  }
+
+  /**
+   * Given a parsed json object and list of paths to map-type fields, descend through the
+   * structure and replace all map-type fields with an equivalent list of key/value pairs.
+   */
+  public static void transformMapFields(TableRow row, List<List<String>> mapFieldPaths) {
+    mapFieldPaths.forEach(path -> {
+      List<String> parentPath = new ArrayList<>(path);
+      String targetFieldName = parentPath.remove(path.size() - 1);
+      // Since the path was determined from the BQ schema where maps were already transformed,
+      // we have to remove elements named "value".
+      parentPath = parentPath.stream().filter(n -> !"value".equals(n)).collect(Collectors.toList());
+      Stream<Map<String, Object>> stream = ImmutableList.<Map<String, Object>>of(row).stream();
+      for (String name : parentPath) {
+        stream = stream.map(f -> f.get(name)).flatMap(f -> {
+          if (f == null) {
+            return ImmutableList.<Object>of().stream();
+          } else if (f instanceof List) {
+            return ((List<Object>) f).stream();
+          } else {
+            return ImmutableList.of((Object) f).stream();
+          }
+        }).filter(Map.class::isInstance).map(f -> (Map<String, Object>) f);
+      }
+      stream.forEach(parent -> {
+        Object target = parent.get(targetFieldName);
+        if (target instanceof Map) {
+          List<ImmutableMap<String, Object>> modified = ((Map<String, Object>) target).entrySet()
+              .stream()
+              .map(entry -> ImmutableMap.of("key", entry.getKey(), "value", entry.getValue()))
+              .collect(Collectors.toList());
+          parent.put(targetFieldName, modified);
+        }
+      });
+    });
   }
 
 }
